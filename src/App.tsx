26dc6d290fe9bd9da960693f2e3503e82bf38907
Doguhan Ozgur Akca <doguhanozgurakca@gmail.com>
2023-03-07 17:28:18 +0300

    tutorial

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { Refine } from "@pankod/refine-core";
+import {
+  Layout,
+  ReadyPage,
+  ErrorComponent,
+  LightTheme,
+  CssBaseline,
+  GlobalStyles,
+  ThemeProvider,
+  RefineSnackbarProvider,
+  notificationProvider,
+} from "@pankod/refine-mui";
+import routerProvider from "@pankod/refine-react-router-v6";
+import dataProvider from "@pankod/refine-simple-rest";
+import { MuiInferencer } from "@pankod/refine-inferencer/mui";
+
+const App: React.FC = () => {
+  return (
+    <ThemeProvider theme={LightTheme}>
+      <CssBaseline />
+      <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
+      <RefineSnackbarProvider>
+        <Refine
+          routerProvider={routerProvider}
+          dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+          notificationProvider={notificationProvider}
+          Layout={Layout}
+          ReadyPage={ReadyPage}
+          catchAll={<ErrorComponent />}
+          resources={[
+            {
+              name: "products",
+              list: MuiInferencer,
+              show: MuiInferencer,
+              create: MuiInferencer,
+              edit: MuiInferencer,
+            },
+          ]}
+        />
+      </RefineSnackbarProvider>
+    </ThemeProvider>
+  );
+};
+
+export default App;
